9ea6847105afc445729a3217dccbe87bdf85198a
Cindy Colucy <ckcolucy@gmail.com>
2017-09-06 14:49:36 -0400

    "gender game"

--- src/pbd/GenderGame.java
+++ src/pbd/GenderGame.java
@@ -7,7 +7,7 @@ public class GenderGame {
 	public static void main(String[] args) {
 
 		Scanner input = new Scanner(System.in);
-		
+
 		System.out.println("What is your gender? (M or F)");
 		String gender = input.nextLine();
 
@@ -19,10 +19,17 @@ public class GenderGame {
 
 		System.out.println("Enter your age.");
 		int age = input.nextInt();
-		
-		if(age < 20) {
-			System.out.println("I shall call you " + firstName + lastName);
-		}
 
+		if (gender == "F" && age < 20) {
+			System.out.println("I shall call you " + firstName + " " + lastName);
+		}
+		if (gender == "F" && age >= 20) {
+			System.out.println("Are you married? (Y or N)");
+			String married = input.nextLine();
+			if (married == "Y") {
+				System.out.println("I shall call you Mrs. " + lastName);
+			}
+		}
 	}
+
 }
