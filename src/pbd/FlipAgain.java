e0e207516396b6acfc4390cf10b19974c2bb1516
Cindy Colucy <ckcolucy@gmail.com>
2017-09-07 12:34:18 -0400

    "Flip Fail"

---
+++ src/pbd/FlipAgain.java
@@ -0,0 +1,30 @@
+package pbd;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class FlipAgain {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		Random rng = new Random();
+
+		String again;
+
+		while (again.equals("y")) {
+			int flip = rng.nextInt(2);
+			String coin;
+
+			if (flip == 1)
+				coin = "HEADS";
+			else
+				coin = "TAILS";
+
+			System.out.println("You flip a coin and it is... " + coin);
+
+			System.out.print("Would you like to flip again (y/n)? ");
+			again = input.next();
+		}
+	}
+
+}
