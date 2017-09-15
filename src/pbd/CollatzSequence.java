4b6e8d79720b8bb849049dc28d72434c8699a3bd
Cindy Colucy <ckcolucy@gmail.com>
2017-09-15 12:01:19 -0400

    "collatz"

--- src/pbd/CollatzSequence.java
+++ src/pbd/CollatzSequence.java
@@ -7,20 +7,22 @@ public class CollatzSequence {
 	public static void main(String[] args) {
 
 		Scanner input = new Scanner(System.in);
+		
+		int counter = 0;
 
 		System.out.println("Enter a number.");
 		int n = input.nextInt();
 
-		while (n % 2 == 0) {
-			System.out.println(n / 2);
-			
-		}
-	
-		while (n % 2 != 0) {
-			System.out.println(3 * n + 1);
-			
-		}
+		while (n != 1) {
+			if (n % 2 == 0) {
+				System.out.println(n / 2 + " ");
+				counter++;
 
+			} else {
+				System.out.println(3 * n + 1 + " ");
+				counter++;
+			}
+
+		}
 	}
-
 }
