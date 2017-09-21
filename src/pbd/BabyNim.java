19a7ac27ddb8818dcbb560086e74388261bf5cad
Cindy Colucy <ckcolucy@gmail.com>
2017-09-21 12:56:05 -0400

    "start Baby Nim"

---
+++ src/pbd/BabyNim.java
@@ -0,0 +1,21 @@
+package pbd;
+
+import java.util.Scanner;
+
+public class BabyNim {
+
+	public static void main(String[] args) {
+
+		int a = 3;
+		int b = 3;
+		int c = 3;
+		
+		Scanner input = new Scanner(System.in);
+
+		System.out.println("Select a pile A, B or C.");
+
+		String pile = input.nextLine();
+
+	}
+
+}
